703d8c5f9b679bef90cdde15b433958c4cc51001
dlekgp1183 <dlekgp1183@naver.com>
2026-05-07 16:06:23 +0900

    first modification

--- project/app.py
+++ project/app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import base64
+from pathlib import Path
+
+# 현재 app.py 기준 경로
+BASE_DIR = Path(__file__).parent
+
+# 이미지 경로
+HOME_PATH = BASE_DIR / "home.png"
 
 # 페이지 설정
 st.set_page_config(
@@ -12,13 +19,12 @@ def get_base64(image_path):
     with open(image_path, "rb") as img:
         return base64.b64encode(img.read()).decode()
 
-home_img = get_base64("home.png")
+home_img = get_base64(HOME_PATH)
 
 # HTML + CSS
 st.markdown(f"""
 <style>
 
-/* 전체 기본 설정 */
 html, body, [data-testid="stAppViewContainer"] {{
     margin: 0;
     padding: 0;
@@ -26,26 +32,19 @@ html, body, [data-testid="stAppViewContainer"] {{
     background: black;
 }}
 
-/* 상단 스트림릿 헤더 제거 */
 header {{
     display: none;
 }}
 
-/* 기본 여백 제거 */
 .block-container {{
     padding: 0 !important;
     max-width: 430px;
     margin: auto;
 }}
 
-/* 핸드폰 화면 */
 .phone {{
-    position: relative;
-
     width: 100%;
     height: 100vh;
-
-    overflow: hidden;
 
     background-image: url("data:image/png;base64,{home_img}");
     background-size: cover;
